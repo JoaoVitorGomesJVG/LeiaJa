06fb7956b56b88256aa1c29e38935afdbf305879
JoaoVitorGomesJVG <gomesjoaovitor007@hotmail.com>
2023-10-11 21:27:43 -0300

    3

--- Leia Já/main.js
+++ Leia Já/main.js
@@ -1,7 +1,3 @@
 document.getElementById("header_button").onclick = function () {
     location.href = "search.html";
 }
-
-document.getElementById("main_title_button").onclick = function () {
-    location.href = "index.html";
-}
