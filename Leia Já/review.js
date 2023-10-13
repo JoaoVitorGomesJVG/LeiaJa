513f7746bb5cb12f53b16e47fd1a15af53d01ad6
JoaoVitorGomesJVG <gomesjoaovitor007@hotmail.com>
2023-10-13 17:09:05 -0300

    9

--- Leia Já/review.js
+++ Leia Já/review.js
@@ -1,6 +1,6 @@
 function showhide()
 {
-      var div = document.getElementById("info_synopsis_container");
+    var div = document.getElementById("info_synopsis_container");
 if (div.style.display !== "block") {
    div.style.display = "block";
 }
